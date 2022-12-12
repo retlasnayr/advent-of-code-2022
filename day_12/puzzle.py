15ab442a645290aa22bdded9fb000f1293572974
Ryan Salter <89457004+retlasnayr@users.noreply.github.com>
2022-12-12 15:47:57 +0000

    Cleanup and complete day 12

--- day_12/puzzle.py
+++ day_12/puzzle.py
@@ -1,5 +1,6 @@
 from utils import read_in_file, print_output, draw_grid, Pair, DIRECTION_VECTORS
 
+
 OPPOSITE = {
     "U": "D",
     "D": "U",
@@ -11,6 +12,91 @@ def compare_heights(current, target):
     return ord(target) - ord(current) <= 1
 
 
+class Node:
+    def __init__(self, height, coords, out_links=None, in_links=None, distance=1e7, sp_tree=False):
+        self.height = height
+        self.coords = coords
+        self.out_links = [] if in_links is None else out_links
+        self.in_links = [] if in_links is None else in_links
+        self.distance = distance
+        self.visited = False
+        self.cheapest_path = []
+
+
+class Graph:
+    def __init__(self, nodes={}, edges=[]):
+        self.nodes = {n.coords: n for n in nodes.values()}
+        self.edges = edges
+        self.start = None
+        self.end = None
+
+    def add_node(self, height, coords, out_links=[]):
+        if height == "S":
+            self.start = coords
+            self.nodes[coords] = Node("a", coords, out_links, distance=0, sp_tree=True)
+            self.nodes[coords].cheapest_path = [coords]
+            return
+        if height == "E":
+            self.end = coords
+            height = "z"
+        self.nodes[coords] = Node(height, coords, out_links)
+
+    def populate_links(self):
+        for node in self.nodes.values():
+            adjacent_nodes = [node.coords + dir_vector for dir_vector in DIRECTION_VECTORS.values()]
+            for adj_node in adjacent_nodes:
+                if adj_node in self.nodes:
+                    if ord(self.nodes[adj_node].height) - 1 <= ord(node.height):
+                        node.out_links.append(adj_node)
+
+    def reverse_links(self):
+        for node in self.nodes.values():
+            adjacent_nodes = [node.coords + dir_vector for dir_vector in DIRECTION_VECTORS.values()]
+            for adj_node in adjacent_nodes:
+                if adj_node in self.nodes:
+                    if ord(self.nodes[adj_node].height) + 1 >= ord(node.height):
+                        node.in_links.append(adj_node)
+
+    def find_route(self, current_node=None):
+        while current_node != self.end:
+
+            if current_node is None:
+                current_node = self.nodes[self.start]
+            else:
+                current_node = self.nodes[current_node]
+            current_node.visited = True
+            for adj_node_coords in current_node.out_links:
+                adj_node = self.nodes[adj_node_coords]
+                if current_node.distance + 1 < adj_node.distance:
+                    adj_node.distance = current_node.distance + 1
+                    adj_node.cheapest_path = current_node.cheapest_path
+                    adj_node.cheapest_path.append(current_node.coords)
+            to_visit = {node.coords: node.distance for node in self.nodes.values() if not node.visited}
+            if not to_visit:
+                break
+            current_node = min(to_visit, key=to_visit.get)
+
+    def find_reverse(self, current_node=None):
+        to_visit = {node.coords: node.distance for node in self.nodes.values() if not node.visited}
+        while to_visit:
+            if current_node is None:
+                current_node = self.nodes[self.end]
+            else:
+                current_node = self.nodes[current_node]
+            current_node.visited = True
+            for adj_node_coords in current_node.in_links:
+                adj_node = self.nodes[adj_node_coords]
+                if current_node.distance + 1 < adj_node.distance:
+                    adj_node.distance = current_node.distance + 1
+                    adj_node.cheapest_path = current_node.cheapest_path
+                    adj_node.cheapest_path.append(current_node.coords)
+            to_visit = {node.coords: node.distance for node in self.nodes.values() if not node.visited}
+            if not to_visit:
+                break
+            current_node = min(to_visit, key=to_visit.get)
+
+
+
 class Grid:
     def __init__(self, lines):
         self.height = None
@@ -18,14 +104,24 @@ class Grid:
         self.data = None
         self.start_location = None
         self.end_location = None
-        self.from_lines(lines)
-        self.find_start_end()
         self.routes = []
+        self.graph = Graph()
+
+        self.from_lines(lines)
+
 
     def from_lines(self, lines):
         self.data = [[x for x in line] for line in lines]
         self.width = len(lines[0])
         self.height = len(lines)
+        self.build_graph()
+
+    def build_graph(self):
+        for row_index, row in enumerate(self.data):
+            for col_index, col in enumerate(row):
+                self.graph.add_node(col, Pair(col_index, row_index))
+        self.graph.populate_links()
+        self.graph.reverse_links()
 
     def row(self, n):
         return self.data[n]
@@ -42,45 +138,6 @@ class Grid:
         loc = current_location + DIRECTION_VECTORS[direction]
         return loc, self.cell_value(loc)
 
-    def find_start_end(self):
-        for row_index, row in enumerate(self.data):
-            for col_index, col in enumerate(row):
-                if col == "S":
-                    self.start_location = Pair(col_index, row_index)
-                    self.data[row_index][col_index] = "a"
-                    continue
-                if col == "E":
-                    self.end_location = Pair(col_index, row_index)
-                    self.data[row_index][col_index] = "z"
-
-    def find_route(self, route_so_far=None):
-        if route_so_far is None:
-            for direction in ("U", "D", "L", "R"):
-                return self.find_route([self.start_location, self.start_location + DIRECTION_VECTORS[direction]])
-        for direction in ("U", "D", "L", "R"):
-            try:
-                test_target = self.get_adjacent(direction, route_so_far[-1])
-                # if test_target[0] == self.end_location:
-                #     route_so_far.append(self.end_location)
-                #     return route_so_far
-            except ValueError:
-                continue
-            if test_target[0] in route_so_far:
-                continue
-            if compare_heights(self.cell_value(route_so_far[-1]), test_target[1]):
-                new_route = route_so_far + [Pair(*test_target[0])]
-                if new_route[-1] == self.end_location:
-                    # if route_so_far not in self.routes:
-                    self.routes.append(new_route)
-                    continue
-                else:
-                    self.find_route(route_so_far=new_route)
-                    continue
-
-
-
-
-
     def print_route(self, route):
         grid_pic = [['.' for _ in range(self.width)] for _ in range(self.height)]
         for num, pair in enumerate(route[:-1]):
@@ -94,18 +151,25 @@ class Grid:
 def part_1(file_path):
     lines = read_in_file(file_path)
     grid = Grid(lines)
-    grid.find_route()
-    print(draw_grid(grid.data))
-    print("\n")
-    print(grid.print_route(grid.routes[0]))
-    print(grid.routes)
-    return(min([len(x) for x in grid.routes]) -1)
+    grid.graph.find_route()
+    return grid.graph.nodes[grid.graph.end].distance
+    # print("\n")
+    # # print(grid.print_route(grid.routes[0]))
+    # # print(grid.routes)
+    # return(min([len(x) for x in grid.routes]) -1)
 
 
 def part_2(file_path):
-    return ""
+    lines = read_in_file(file_path)
+
+    clean_lines = [line.replace("S", "a") for line in lines]
+    grid = Grid(lines)
+    grid.graph.nodes[grid.graph.end].distance = 0
+    grid.graph.nodes[grid.graph.start].distance = 1e5
+    grid.graph.find_reverse(grid.graph.end)
+    lengths = [x.distance for x in grid.graph.nodes.values() if x.height == "a"]
+    return min(lengths)
 
 
 if __name__ == "__main__":
     print_output(part_1, part_2)
-
